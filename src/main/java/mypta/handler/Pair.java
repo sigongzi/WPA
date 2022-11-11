ee26f442825e9d1f2368c72fd8b4b8114ee8b95c
wrc042 <wrc0326@outlook.com>
2022-11-11 22:03:52 +0800

    fix Pair

--- src/main/java/mypta/handler/Pair.java
+++ src/main/java/mypta/handler/Pair.java
@@ -2,15 +2,18 @@ package mypta.handler;
 
 import mypta.util.benchmark.TestId;
 
-public class Pair<T1,T2>{
+public class Pair<T1, T2> {
     T1 obj1;
     T2 obj2;
+
     public T1 getFirst() {
         return obj1;
     }
+
     public T2 getSecond() {
         return obj2;
     }
+
     public Pair(T1 a, T2 b) {
         obj1 = a;
         obj2 = b;
@@ -18,6 +21,14 @@ public class Pair<T1,T2>{
 
     @Override
     public String toString() {
-        return String.format("Pair: <OBJ1>.%s <OBJ2>.%s", obj1.toString(), obj2.toString());
+        String str1 = "null";
+        if (obj1 != null) {
+            str1 = obj1.toString();
+        }
+        String str2 = "null";
+        if (obj2 != null) {
+            str2 = obj2.toString();
+        }
+        return String.format("<%s, %s>", str1, str2);
     }
 }
